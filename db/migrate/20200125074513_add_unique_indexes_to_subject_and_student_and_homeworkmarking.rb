c03d4b9fe11889578655b626cd8e7088b6ae009e
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2020-01-25 17:18:20 +0900

    Index name length is limited to be under 63 characters

--- db/migrate/20200125074513_add_unique_indexes_to_subject_and_student_and_homeworkmarking.rb
+++ db/migrate/20200125074513_add_unique_indexes_to_subject_and_student_and_homeworkmarking.rb
@@ -2,6 +2,6 @@ class AddUniqueIndexesToSubjectAndStudentAndHomeworkmarking < ActiveRecord::Migr
   def change
     add_index :students, [:school_id, :student_id_number], unique: true
     add_index :subjects, [:school_id, :name], unique: true
-    add_index :homework_markings, [:homework_id, :homework_question_id, :student_id]
+    add_index :homework_markings, [:homework_id, :homework_question_id, :student_id], name: 'idx_homeworkid_homeworkquestionid_studentid'
   end
 end
